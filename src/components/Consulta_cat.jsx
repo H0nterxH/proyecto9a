03d9225f5047f44ac56336649dfd3eca5aa79424
H0nterxH <utp0145470@alumno.utpuebla.edu.mx>
2023-08-03 14:33:50 -0600

    consulta categoria

--- src/components/Consulta_cat.jsx
+++ src/components/Consulta_cat.jsx
@@ -1,69 +1,96 @@
-import React from 'react';
-const Content = () => {
-  return (
-    <>
-        <div className="content-wrapper">
-            <div className="content-header">
-                <div className="container">
-                    <div className="row mb-2">
-                    <div className="col-sm-6">
-                        <h1 className="m-0">Consulta de Productos </h1>
-                    </div>
-                    
-                    </div>
-                </div>
-            </div>
-            <div className="content">
-                <div className="container ">
-                    
-                    <div className='row'>
-                        <div className='col-xs-12  col-lg-12'>
-                            <div className='card card-primary'>
-                                <div className='card-header'>
-                                    <h4 className='card-title'>
-                                        <i className="fas mr-2"></i>
-                                        Tabla de categorias
-                                    </h4>
-                                </div>
-                                  <div > 
-                                      <table id="tablaconsultausu" className='table text-center'>
-                                          <tr>
-                                              <th scope="col">Idcategoria</th>
-                                              <th scope="col">Tipo</th>
-                                              <th scope="col">Nombre</th>
-                                              <th scope="col">Status</th>
-                                              <th scope="col">Fecha de alta</th>
-                                              <th scope="col">Personal</th>
-                                              <th scope="col">Acciones</th>
-                                              
-                                          </tr>
-                                          <tr>
-                                              <th scope="col"></th>
-                                              <th scope="col"></th>
-                                              <th scope="col"></th>
-                                              <th scope="col"></th>
-                                              <th scope="col"></th>
-                                              <th scope="col"></th>
-                                              <th scope="col">
-                                                  <button className='btn btn-sm bg-blue  col-lg-4 offset-md-1'>Editar</button>
-                                                  <button className='btn btn-sm bg-danger col-lg-4 offset-md-1'>Borrar</button></th>
-                                          </tr>
-                                          
+import { useEffect, useState } from "react";
 
-                                      </table>
-                                  </div>
-                                
+function Consulta_pro() {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost/dwi-9a/index.php/Api/Categoria');
+                if (!response.ok) {
+                    throw new Error('La solicitud no pudo ser completada.');
+                }
+                const data = await response.json();
+                setData(data);
+                setIsLoading(false);
+            } catch (error) {
+                setError(error.message);
+                setIsLoading(false);
+            }
+        };
+
+        
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <div>Cargando...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+    return (
+        <>
+            <div className="content-wrapper">
+                <div className="content-header">
+                </div>
+                <div className="content">
+                    <div className="container">
+
+                        <div className='row'>
+                            <div className='col-xs-12  col-lg-12'>
+                                <div className='card card-primary'>
+                                    <div className='card-header'>
+                                        <h4 className='card-title'>
+                                            Consulta de Categorias
+                                        </h4>
+                                    </div>
+                                    <div >
+                                        <table className='table text-center'>
+                                            <thead>
+                                                <tr>
+                                                    <th>Idcategoria</th>
+                                                    <th>Tipo</th>
+                                                    <th>Nombre</th>
+                                                    <th>Status</th>
+                                                    <th>Fecha de alta</th>
+                                                    <th>Personal</th>
+                                                    <th className="col-lg-5">Acciones</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {data.map((item) => (
+                                                    <tr key={item.IdCategoria}>
+                                                        <td>{item.Tipo}</td>
+                                                        <td>{item.Nombre}</td>
+                                                        <td>{item.Status}</td>
+                                                        <td>{item.Fecha_alta}</td>
+                                                        <td>{item.Personal}</td>
+                                                        <td >
+                                                            <button className='btn btn-sm bg-blue col-lg-4 offset-md-1'>Editar</button>
+                                                            <button className='btn btn-sm bg-danger col-lg-4 offset-md-1 '>Borrar</button>
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
                             </div>
+
+
                         </div>
 
-                        
                     </div>
-                    
                 </div>
             </div>
-        </div>
-    </>
-  )
-}
 
-export default Content;
+       </>
+    );
+};
+
+export default Consulta_cat;
